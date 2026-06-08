074950ddad6b1a77e9d9888975ffab8a58cf995e
mistic100 <contact@git.strangeplanet.fr>
2026-06-08 19:01:03 +0200

    Fix "video" must be a list + existence check is not useful

--- source/notify_tubearchivist/plugin.py
+++ source/notify_tubearchivist/plugin.py
@@ -21,6 +21,7 @@
 """
 import logging
 import requests
+from typing import cast
 from pathlib import Path
 from unmanic.libs.unplugins.settings import PluginSettings
 
@@ -33,42 +34,29 @@ class Settings(PluginSettings):
         "TubeArchivist API Token": "",
     }
 
-def ta_video_exists(ta_url: str, ta_token: str, video_id: str) -> bool:
-    headers = {
-        "Authorization": f"Token {ta_token}",
-        "Content-Type": "application/json"
-    }
-
-    try:
-        r = requests.get(f"{ta_url}/api/video/{video_id}", headers=headers, timeout=10)
-        return r.status_code == 200
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Error connecting to TubeArchivist API: {e}")
-        return False
-
-def notify_ta(ta_url: str, ta_token: str, video_id: str):
+def notify_ta(ta_url: str, ta_token: str, video_ids: list[str]):
     headers = {
         "Authorization": f"Token {ta_token}",
         "Content-Type": "application/json"
     }
 
     payload = {
-        "video": video_id
+        "video": video_ids
     }
 
     try:
         r = requests.post(f"{ta_url}/api/refresh/", json=payload, headers=headers, timeout=10)
 
         if r.status_code == 200:
-            logger.info(f"Successfully triggered TubeArchivist refresh for {video_id}")
+            logger.info(f"Successfully triggered TubeArchivist refresh for {video_ids}.")
         else:
-            logger.error(f"Failed to trigger TubeArchivist refresh. Status code: {r.status_code}, Response: {r.text}")
+            logger.error(f"Failed to trigger TubeArchivist refresh for {video_ids}. Status code: {r.status_code}, Response: {r.text}.")
     except requests.exceptions.RequestException as e:
-        logger.error(f"Error connecting to TubeArchivist API: {e}")
+        logger.error(f"Error connecting to TubeArchivist API: {e}.")
 
 def on_postprocessor_task_results(data: dict):
     if not data.get('destination_files'):
-        loggloggering.info('No destination files')
+        logger.info('No destination files')
         return data
 
     if data.get('library_id'):
@@ -76,19 +64,14 @@ def on_postprocessor_task_results(data: dict):
     else:
         settings = Settings()
 
-    ta_url = settings.get_setting('TubeArchivist URL')
-    ta_token = settings.get_setting('TubeArchivist API Token')
+    ta_url = cast(str, settings.get_setting('TubeArchivist URL'))
+    ta_token = cast(str, settings.get_setting('TubeArchivist API Token'))
 
     if not ta_url or not ta_token:
-        logger.warning("TubeArchivist URL/API Token is not configured, skipping")
+        logger.error("TubeArchivist URL/API Token is not configured.")
         return data
-
-    for file in data.get('destination_files'):
-        video_id = Path(file).stem
-
-        if ta_video_exists(ta_url, ta_token, video_id):
-            notify_ta(ta_url, ta_token, video_id)
-        else:
-            logger.warning(f"Video {video_id} does not exist on TubeArchivist")
+    
+    video_ids = [Path(file).stem for file in data.get('destination_files', [])]
+    notify_ta(ta_url, ta_token, video_ids)
 
     return data
